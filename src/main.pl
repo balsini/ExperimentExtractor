998629463f34c6f680ee58dbbe3714bf252aa8fa
Alessio Balsini <a.balsini@sssup.it>
2016-04-12 01:18:44 +0200

    Reorganization

--- src/main.pl
+++ src/main.pl
@@ -7,8 +7,7 @@ use File::Slurp qw(read_dir);
 my $base_path = '/home/alessio/git/rtlib2.0/build/examples/accelerated_full/results/';
 my $filename = 'output.txt';
 
-my @runs = sort { $a cmp $b } grep { -d "$base_path/$_" } read_dir($base_path);
-
+my @runs = sort { -M "$base_path/$a" <=> -M "$base_path/$b" } grep { -d "$base_path/$_" } read_dir($base_path);
 my $root = $base_path . $runs[0]; 
 
 sub deadline_miss_file
@@ -32,13 +31,10 @@ sub deadline_miss_file
 		
 		if (scalar(@columns) gt $relative_RT_column) {
 			
-			#print "here";
-			
 			if ($columns[$relative_RT_column] gt '1.0') {
 				return 1;
 			}
 			
-			#print "here\n";
 		}
 	}
 	return 0;
@@ -47,13 +43,14 @@ sub deadline_miss_file
 
 # For each experiment 
 for my $experiment (sort { $a cmp $b } grep { -d "$root/$_" } read_dir($root)) {
-    print "Experiment: $experiment\n";
+    print "$experiment\t SchedulableTasksets\n";
     
     my $experiment_root = $root . '/' . $experiment;
     
     # For each experiment parameter
     for my $x (sort { $a cmp $b } grep { -d "$experiment_root/$_" } read_dir($experiment_root)) {
-    	print "\tx: $x\n";
+    	#print "\tx: $x\n";
+    	print "$x\t";
     	
     	my $result_root = $experiment_root . '/' . $x;
     	
@@ -62,7 +59,6 @@ for my $experiment (sort { $a cmp $b } grep { -d "$root/$_" } read_dir($root)) {
     	my $samples = 0;
     	for my $result (sort { $a cmp $b } grep { -d "$result_root/$_" } read_dir($result_root)) {
 	    	
-	    	
 	    	my $file_path = $result_root . '/' . $result . '/' .$filename;
 			open(my $fh, '<:encoding(UTF-8)', $file_path)
 			  or die "Could not open file '$file_path' $!";
@@ -70,7 +66,7 @@ for my $experiment (sort { $a cmp $b } grep { -d "$root/$_" } read_dir($root)) {
 			$dlm = $dlm + deadline_miss_file($fh);
 			$samples = $samples + 1;
 	    }
-	    my $succeeded = $samples - $dlm;
-	    print "\t\tSucceeded: $succeeded / $samples\n";
+	    my $succeeded = ($samples - $dlm) / $samples;
+	    print "$succeeded\n";
     }
 }
